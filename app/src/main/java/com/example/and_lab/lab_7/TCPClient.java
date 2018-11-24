691fee913a7d55f440c83062a1df55d72474c54d
Ibram Medhat <ibram.medhat2@gmail.com>
2018-11-24 16:00:49 +0100

    Adding constructor and start method to TCPClient class

--- app/src/main/java/com/example/and_lab/lab_7/TCPClient.java
+++ app/src/main/java/com/example/and_lab/lab_7/TCPClient.java
@@ -3,11 +3,19 @@ import java.io.*;
 import java.net.*;
 
 class TCPClient {
-    public static void main(String argv[]) throws Exception {
+    private int port_number;
+
+    public TCPClient(int port_number){
+        this.port_number = port_number;
+    }
+
+    public void Start() throws IOException {
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
+
+       Socket clientSocket = new Socket("10.0.2.2", port_number);
+
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();
@@ -15,5 +23,9 @@ class TCPClient {
         modifiedSentence = inFromServer.readLine();
         System.out.println("FROM SERVER: " + modifiedSentence);
         clientSocket.close();
+
     }
+
+    public static void main(String argv[]) throws Exception {
+        }
 }
